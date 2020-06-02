f62560bf48177a19d1f69c07a14469c17a5c4b34
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-02 02:30:27 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,14 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  
+  contacts.each do |name, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        value.delete("strawberry")
+      end
+    end
+  end
+  
 
 end
